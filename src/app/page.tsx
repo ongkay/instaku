e9396446f73c349e5341319c7ac483df2bee86d5
ongkay <bisnismilyarandollar@gmail.com>
2023-06-08 11:43:20 +0700

    refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Welcome } from '../components/Welcome/Welcome'
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle'
 import { InstaReg } from '../components/InstaReg'
 
