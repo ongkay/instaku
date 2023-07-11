f8e7c488ccb697c8529242c9fd85d9f699c6b65b
ongkay <bisnismilyarandollar@gmail.com>
2023-07-11 17:47:25 +0700

    fix lahir 30

--- src/lib/getRandomUser.ts
+++ src/lib/getRandomUser.ts
@@ -23,7 +23,7 @@ export function getRandomUser() {
 }
 
 function deteklahir() {
-    const nilai = ["satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "lapan", "sembilan", 'sepuluh', 'sebelas', 'duabelas'];
+    const nilai = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"];
     const idx = (Math.random() * nilai.length) | 0;
     return nilai[idx]
 };
